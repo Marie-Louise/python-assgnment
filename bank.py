0cb659b9f2c8b1511a8d501995c811cde154e812
Marie-Louise <79199597+Marie-Louise@users.noreply.github.com>
2021-06-11 23:02:08 +0300

    Update bank.py

--- bank.py
+++ bank.py
@@ -9,33 +9,38 @@ class Account:
     self.loan=0
     self.statement=[]
    def deposit(self,amount):
-      if amount >  0:
-          self.balance+=amount
-          now=datetime.now() 
-          transaction={
-             "amount":amount,
-             "time":now,
-             "narration":"you haave deposited"
-         }
-          self. statement.append(transaction)
-          return self.show_balance
+      try:
+            5/amount
+      except TypeError:
+            print  ("cant divide by string")        
+      if amount <  0:
+         return 'please input a positive amount'
        
       else :
-         return  f"Dear {self.name} yor deposit is not enough"        
-           
+         self.balance+=amount
+         now=datetime.now() 
+      transaction={
+             "amount":amount,
+             "time":now,
+             "narration":"you haave deposited"}
+      self.statement.append(transaction)
+      return f'you have deposited  {amount} and your balance is {self.balance}'          
    def get_statement(self):
-      for  transaction in self.transactions:
+      for  transaction in self.statement:
          narration =transaction ["narration"]
          amount = transaction["amount"]
          balance=transaction["balance"]
          time=transaction["time"]
          date=time.strftime("%d/%m/%y")
-         print(f"{date}:{narration}  {amount}")
-         
-         return 
-        
+         print(f"{date} {narration} {balance} {amount}")
+     
                
    def withdrow(self,amount):
+      try:
+            5/amount
+      except TypeError:
+             print ("cant divide by string")          
+          
       if amount<0:
        return f"Dear {self.name} You can't borrow negative amount"
       elif self.balance<amount:
@@ -55,12 +60,17 @@ class Account:
           self.balance+=amount
       return f"Dear {self.name}, You have borrowed {amount} your new balance is{self.balance}"  
    def repay(self,amount):
-         if amount > 0:
+       try:
+             5/amount
+       except TypeError:
+               print("cant divide by string") 
+                       
+       if amount > 0:
                  return f"Dear {self.name} your loan is not valid"
-         elif amount>self.loan:
+       elif amount>self.loan:
             self.loan-=amount
             return f"Dear {self.name} your loan has been decreased {self.loan}"
-         else:
+       else:
             diff=amount-self.loan
             self.laon=0
             self.deposit(diff)
@@ -73,8 +83,45 @@ class Account:
                
             } 
             self.statement.append(transaction)
-            return f"Dear {self.name} your loan is cleared"          
+            return f"Dear {self.name} your loan is cleared"   
+   def transfer(self,account,amount):
+      try:
+            3/amount
+      except TypeError:
+            print("please put number") 
+      fee=amount*0.05      
+      if amount<0:
+         return "Please put a positive amount"  
+      elif  amount+fee>self.balance:
+         return f"You have less money on your account. you need {amount+fee} to complete the transaction" 
+      else:
+         self.balance-amount-fee
+         account.deposit(amount)
+         return f"You have sent {amount}  to {account.name} your balance is {self.balance} and you have been charged {fee}"                   
             
+class Mobilemoney(Account):
+   def __init__(self,name,phone,loan_limit,service_provider):
+      Account.__init__(name,phone,loan_limit)
+      self.service_provide=service_provider
+      self.limit=200000
+   def buy_airtime(self,amount):
+       try:
+            5/amount  
+       except TypeError:
+            print("Cant divide a string")    
+       if amount<0:
+          return 'you cant borrow negative airtime'  
+       elif amount>self.balance:
+          return 'you dont have enough balance to buy airtime'
+       else:
+          self.balance-=amount 
+          return f'You have bought {amount} airtime your balance is now {self.balance}' 
+   
+          
+                   
+     
+   
+
                  
      
    
